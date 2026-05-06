29724d2f494ea31484340d575bb465a041372991
Tiver Sahni <tiver.pp@gmail.com>
2026-05-06 18:34:37 +0530

    first commit

---
+++ Consta.ts
@@ -0,0 +1,78 @@
+/*let birth = 45;
+
+if (birth > 18 )
+{
+    console.log( "can be vote" );
+
+}
+
+else
+{
+    console.log( "can not be vote" );
+}
+
+
+let mode = "green"
+let color
+
+  if 
+    
+    (mode === "dark")
+
+    { color = "black"; 
+
+    }
+
+  
+  else
+
+    {
+
+        color= "white"
+    }
+
+    console.log(color);
+
+
+
+    let number =40; 
+    if (number % 2 == 0)
+
+    {
+        console.log("even");
+    }
+    else
+    {
+        console.log("odd");
+    }*/
+
+
+
+    //Else if example 
+
+    let marks = 0;
+
+    if (marks >= 90)
+    {
+        console.log("Grade A");
+    }
+    else if (marks >= 80)
+    {
+        console.log("Grade B");
+    }
+    else if (marks >= 70)
+    {
+        console.log("Grade C");
+    }
+    else if (marks >= 60)
+    {
+        console.log("Grade D");
+    }
+    else if (marks >= 50)
+    {
+        console.log("Grade E");
+    }
+    else
+    {
+        console.log("Grade F");
+    }
---
+++ Hello.js
@@ -0,0 +1,38 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fullName = "Tony Stark";
+console.log(fullName);
+console.log('Test Program');
+const age = 24;
+const price = 99.99;
+const x = null;
+let y;
+//let x1 = age
+/*console.log(x);
+console.log(x1);*/
+const student = {
+    Name: "Tiver Sahni",
+    Age: 36,
+    Cgpa: 8.2,
+    isPass: true
+};
+console.log(student.Age);
+const product = {
+    Pname: "Pen",
+    Rating: 4.5,
+    Offer: 4,
+    Amount: 270,
+};
+console.log(product);
+console.log(typeof product);
+console.log(typeof student.Name);
+const profile = {
+    Name: "Shradha Khapra",
+    Post: 195,
+    Followers: 569000,
+    Following: 4,
+    CollageName: "Apna Collage ",
+    Designation: "Entrepreneur",
+    Follow: true,
+};
+console.log(profile.Follow);
