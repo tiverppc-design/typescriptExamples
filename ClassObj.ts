b8acba0aa1b57b25b2c35f8fd4295e5046502f86
tiverppc-design <tiver.ppc@gmail.com>
2026-05-07 18:53:57 +0530

    7-May-2026 practice

--- ClassObj.ts
+++ ClassObj.ts
@@ -17,3 +17,50 @@ class Test
         console.log("M3     Hello World");
     }
 }
+
+const t1 = new Test();
+t1.M1();
+t1.M2();
+t1.M3();
+
+class test3
+{ 
+ name1 : string ="Tiver Sahni";
+ name2 : string ="Shradha Khapra"
+ age2 : number =36;
+ age1 : number =24;
+
+}
+
+const t3 = new test3();
+console.log(t3.name1);
+console.log(t3.age1);
+console.log(t3.name2);
+console.log(t3.age2);
+
+
+
+
+class test4
+{ 
+    exam() : void 
+    {
+        console.log("Exam is over");
+    }
+
+     exam2() : void
+    {
+        console.log("Passing " + (10 + 20));
+    }
+       marks(engMarks : number, mathMarks : number) : void
+       {
+        console.log("Eng Marks: " + engMarks + ", Math Marks: " + mathMarks);
+        console.log("Total Marks: " + (engMarks + mathMarks));
+       }
+    }
+       
+       const t4 = new test4(); 
+       t4.exam();
+       t4.exam2();
+       t4.marks(10,14);
+   
