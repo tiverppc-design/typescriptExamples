df1b1472847a0300b9a6a64375e9e2f8d615247b
tiverppc-design <tiver.ppc@gmail.com>
2026-05-11 09:29:55 +0530

    update 11 may -2026

---
+++ exp.condition.ts
@@ -0,0 +1,8 @@
+class condition {
+    a : number = 10; 
+    constructor() {
+    if (this.a <= 10) {
+        console.log("a is less than or equal to 10");
+    } }
+}
+let condition1 = new condition();
