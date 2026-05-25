8d15916db680971726ceaec23722b055f8e1a9e3
tiverppc-design <tiver.ppc@gmail.com>
2026-05-25 10:41:27 +0530

    changes 3

--- tests/Opencloseapp.spec.ts
+++ tests/Opencloseapp.spec.ts
@@ -1,6 +1,4 @@
 import {test, expect} from '@playwright/test';
-
-
 const url= 'http://google.com';
 const username : string = 'testuser';
 const password : string = 'testpassword';
@@ -9,45 +7,29 @@ test('open and close app', async ({page}) => {
   await page.waitForTimeout(5000);
   console.log('App opened successfully');   
   // Simulate login by filling in username and password fields
-
   await page.locator('body').fill(username); 
       console.log('Username entered successfully');
-
-
       // Enter Password 
          await  page.locator('body').fill(password);
    console.log('Password entered successfully');
   await page.waitForTimeout(5000);
-
      //click on login button and validate 
         await page.locator('body').click();
     console.log('Login button clicked successfully');
-
-
     // Validate login successfully
-
         await expect(page).toHaveURL('http://google.com');
     console.log('Login validated successfully');
-
   // Mouse hover on Menu 
-
   await page.locator('body').hover();
    console.log('Mouse hovered successfully');
   await page.waitForTimeout(5000);
-  
-   // Click on Submenu  
+     // Click on Submenu  
   await page.getByText('body').click();
    console.log('Mouse clicked successfully');
-
   await page.waitForTimeout(5000);
-
-6
 // Click on Logout button
-
    await page.locator('body').click();    
   console.log('App logout successfully'); 
-
-
 });
 
  
